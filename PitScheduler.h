a01666f977cfa2a53c2a156869fc6e0c4c38aa44
Brndnz123 <brnndnnz@hotmail.com>
2026-03-24 23:10:54 +0300

    Added PitScheduler.h for pit scheduling feature

---
+++ PitScheduler.h
@@ -0,0 +1,14 @@
+#ifndef PITSCHEDULER_H
+#define PITSCHEDULER_H
+
+class PitScheduler {
+public:
+    // Constructor and destructor
+    PitScheduler();
+    ~PitScheduler();
+
+    // Function to schedule mining phases
+    void scheduleMiningPhases();
+};
+
+#endif // PITSCHEDULER_H
